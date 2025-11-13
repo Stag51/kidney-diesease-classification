7be5ed04476dbf2d5872ab934c63c5b2a5400b07
Stag51 <dev.saad.shabbir@gmail.com>
2025-11-13 19:51:14 +0500

    Prepare base model added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -30,16 +30,16 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
-class ConfigurationManager:
-    def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
+# class ConfigurationManager:
+#     def __init__(
+#         self,
+#         config_filepath = CONFIG_FILE_PATH,
+#         params_filepath = PARAMS_FILE_PATH):
 
-        self.config = read_yaml(config_filepath)
-        self.params = read_yaml(params_filepath)
+#         self.config = read_yaml(config_filepath)
+#         self.params = read_yaml(params_filepath)
 
-        create_directories([self.config.artifacts_root])
+#         create_directories([self.config.artifacts_root])
 
     
 
